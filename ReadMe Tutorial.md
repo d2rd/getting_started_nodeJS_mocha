9731192cc927fd17ff4b0c3f087dc9e53ffe6810
Glenn-David Daniel <d2rd@outlook.com>
2017-09-11 17:36:19 -0700

    Tests written

---
+++ ReadMe Tutorial.md
@@ -0,0 +1,9 @@
+<!--The following command is used to invoke the Mocha binary installed locally in the ./node_modules directory:
+
+./node_modules/.bin/mocha --reporter spec
+
+Note that we have also changed the standard reporter that Mocha uses, and set up the spec reporter that is more verbose, and thus more suitable for beginners. You can explore other reporters on Mocha's official website.
+
+
+-->
+
---
+++ converter/test/converter.js
@@ -0,0 +1,32 @@
+var expect = require("chai").expect;
+var converter = require("../app/converter");
+
+describe("color Code Converter", function() {
+
+
+
+  describe("rgb to hex conversion", function() {
+    it("converts the basic colors", function() {
+      var redHex = converter.rgbToHex(255, 0,0);
+      var grenHex = converter.rgbToHex(0,255,0);
+      var blueHex = converter.rgbToHex(0, 0,255);
+
+      expect(redHext).to.equal("ff0000");
+      expect(greenHex).to.equal("00ff00");
+      expect(blueHex).to.equal("0000ff");
+    });
+  });
+
+  describe ("Hex to RGB conversion", function(){
+    it("converts the basic colors", function() {
+      var red   = converter.hexToRgb("ff0000");
+      var green = converter.hexToRgb("00ff00");
+      var blue  = converter.hexToRgb("0000ff");
+
+      expect(red).to.deep.equal([255, 0, 0]);
+      expect(green).to.deep.equal([0, 255, 0]);
+      expect(blue).to.deep.equal([0, 0, 255]);
+    });
+  });
+});
+
